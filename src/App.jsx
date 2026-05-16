e145eb7fdbef2ee111fbb17997573a56d408007c
cakethehuman <willsonputra17@gmail.com>
2026-05-16 22:18:53 +0700

    Fix some stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,38 @@
 import Navbar from './Navbar';
 
 // Main hero page image
-import ameProfile from './assets/ame.jpg';
-import linkedinLogo from './assets/linkedin.svg';
-import githubLogo from './assets/github.svg';
+import ameProfile from './assets/profile/ame.jpg';
+import linkedinLogo from './assets/profile/linkedin.svg';
+import githubLogo from './assets/profile/github.svg';
 
 // Machine learning skills
-import sklearnLogo from './assets/sklearn.svg'
-import tensorflowLogo from './assets/tensorflow.svg'
-import pytrochLogo from './assets/pytorch.svg'
-import statsModelLogo from './assets/statsmodel.svg'
-import openCVLogo from './assets/opencv.svg'
-import tableauLogo from './assets/tableau.svg'
-import numpyLogo from './assets/numpy.svg'
-import pandasLogo from './assets/Pandas.svg'
-import seabornLogo from './assets/seaborn.svg'
+import sklearnLogo from './assets/MLskills/sklearn.svg'
+import tensorflowLogo from './assets/MLskills/tensorflow.svg'
+import pytrochLogo from './assets/MLskills/pytorch.svg'
+import statsModelLogo from './assets/MLskills/statsmodel.svg'
+import openCVLogo from './assets/MLskills/opencv.svg'
+import tableauLogo from './assets/MLskills/tableau.svg'
+import numpyLogo from './assets/MLskills/numpy.svg'
+import pandasLogo from './assets/MLskills/Pandas.svg'
+import seabornLogo from './assets/MLskills/seaborn.svg'
 
-//
-import fastApiLogo from './assets/FastAPI.svg'
-import raectLogo from './assets/react.svg'
+// web dev skills
+import fastApiLogo from './assets/Webskills/FastAPI.svg'
+import raectLogo from './assets/Webskills/react.svg'
+import laravelLogo from './assets/Webskills/laravel.svg'
+import mySQLLogo from './assets/Webskills/mysql.svg'
+import mongodbLogo from './assets/Webskills/mongodb.svg'
+import sqlLiteLogo from './assets/Webskills/sqlite.svg'
+import jsLogo from './assets/Webskills/js.svg'
+import tailwindLogo from './assets/Webskills/tailwind.svg'
+import phpLogo from './assets/Webskills/php.svg'
+
+// expreices fotos
+import soushinLogo from './assets/Experience/shoushin.png'
+// soushin
+import nihongo1 from './assets/Experience/Shoushin/Nihongo2025/foto1.jpeg'
+
+import dcLogo from './assets/Experience/discord.svg'
 
 export default function App() {
   return (
@@ -76,12 +90,14 @@ export default function App() {
           <div className="flex-col border items-center max-w-2xl w-full h-auto bg-gray-800 border-5 border-emerald-500 rounded-3xl shadow-xl shadow-emerald-500/50">
             <div className="grid grid-cols-3 gap-5 my-15 place-items-center">
               <img src={fastApiLogo} alt="FastAPI" className="w-25 h-25 object-contain" />
-              <img src="/assets/svg/js-official-svgrepo-com.svg" alt="JavaScript" className="w-25 h-25 object-contain" />
-              <img src="/assets/svg/mongo-svgrepo-com.svg" alt="MongoDB" className="w-25 h-25 object-contain" />
-              <img src="/assets/svg/mysql-logo-svgrepo-com.svg" alt="MySQL" className="w-25 h-25 object-contain" />
-              <img src="/assets/svg/tailwind-svgrepo-com.svg" alt="Tailwind" className="w-25 h-25 object-contain" />
-              <img src="/assets/svg/laravel-svgrepo-com.svg" alt="Laravel" className="w-25 h-25 object-contain" />
               <img src={raectLogo} alt="Laravel" className="w-25 h-25 object-contain" />
+              <img src={laravelLogo} alt="Laravel" className="w-25 h-25 object-contain" />
+              <img src={mySQLLogo} alt="MySQL" className="w-25 h-25 object-contain" />
+              <img src={mongodbLogo} alt="Mongodb" className="w-25 h-25 object-contain" />
+              <img src={sqlLiteLogo} alt="SQlite" className="w-25 h-25 object-contain" />
+              <img src={jsLogo} alt="JavaScript" className="w-25 h-25 object-contain" />
+              <img src={tailwindLogo} alt="Tailwind" className="w-25 h-25 object-contain" />
+              <img src={phpLogo} alt="PHP" className="w-25 h-25 object-contain" />
             </div>
           </div>
         </div>
@@ -90,29 +106,50 @@ export default function App() {
       {/* Experience Section */}
       <section id="Exprerience">
         <div className="flex justify-center">
-          <h1 className="text-white font-bold text-3xl w-60 h-15 py-2 text-center border-2 bg-gray-800 border-white rounded-full m-5 shadow-xl shadow-white">
+          <h1 className="text-white font-bold text-3xl w-60 h-15 py-2 text-center border-2 bg-gray-800 border-white rounded-full m-7 shadow-xl shadow-white">
             EXPERIENCE
           </h1>
         </div>
         <div className="flex justify-center">
           <div className="border border-3 border-white max-w-7xl w-full h-auto px-25 py-18 bg-gray-800 rounded-2xl shadow-lg shadow-white">
             <div className="flex flex-col gap-10">
+              <div className='border border-3 border-red-700 w-full h-auto px-10 py-5 bg-gray-800 rounded-2xl shadow-lg shadow-red-500 gap-2'>
               <div className="flex flex-row gap-10">
-                <img src="/assets/img/shoushin logo.png" className="w-23 h-25" alt="Shoushin Logo" />
+                <img src={soushinLogo} className="w-23 h-25" alt="Shoushin Logo" />
                 <div className="flex flex-col">
-                  <h1 className="text-white text-3xl">Seketaris 2</h1>
+                  <h1 className="text-white text-3xl">Sekretaris 2</h1>
                   <p className="text-white text-sm">Nov 2025 - Now</p>
-                  <p className="text-white mt-2">Pengalaman Proker : </p>
-                  <ul className="list-disc list-inside ml-2">
-                    <li className="text-white">Koor dana harukaze</li>
-                    <li className="text-white">Anggota Humas</li>
-                    <li className="text-white">Anggota Dana</li>
-                    <li className="text-white">Anggota Dana</li>
-                  </ul>
+                  <div className='flex justify-center'>
+                    <p className="border border-2 rounded-full border-red-700 bg-red-700 w-60 px-5 py-2 text-center text-white text-bold m-2">Pengalaman Proker</p>
+                  </div>
+                    <div className="grid grid-cols-3 gap-7">
+                        {[
+                          { title: "Nihongo Contest 2025", 
+                            job: "Anggota Dana",
+                            desc : "Bantu mendapatkan dana untuk acara",
+                            image: nihongo1 },
+                          { title: "Nihongo Contest 2025", 
+                            job: "Anggota Dana",
+                            desc : "Bantu mendapatkan dana untuk acara",
+                            image: nihongo1 },
+                          { title: "Nihongo Contest 2025", 
+                            job: "Anggota Dana",
+                            desc : "Bantu mendapatkan dana untuk acara",
+                            image: nihongo1 },
+                        ].map((cert, index) => (
+                          <div key={index} className="flex flex-col border border-3 border-red-700 max-w-xl w-full h-auto px-5 py-5 bg-gray-800 rounded-2xl shadow-lg shadow-red-500 gap-2">
+                            <img src={cert.image} alt = {cert.image}/>
+                            <h1 className="text-white text-md font-bold">{cert.title}</h1>
+                            <p className="text-white text-sm font-bold">{cert.job}</p>
+                            <p className="text-white text-xs">{cert.desc}</p>
+                          </div>
+                        ))}
+                      </div>
+                      </div>
                 </div>
               </div>
               <div className="flex flex-row gap-10">
-                <img src="/assets/svg/discord-icon-svgrepo-com.svg" className="w-23 h-25" alt="Discord" />
+                <img src={dcLogo} className="w-23 h-25" alt="Discord" />
                 <div className="flex flex-col"> 
                   <h1 className="text-white text-3xl">Discord Bot Dev</h1>
                   <p className="text-white text-sm">Nov 2024 - Feb 2025</p>
@@ -126,22 +163,25 @@ export default function App() {
       {/* Projects Section */}
       <section id="Projects">
         <div className="flex justify-center">
-          <h1 className="text-white font-bold text-3xl w-60 h-15 py-2 text-center border-2 bg-gray-800 border-blue-500 rounded-full m-5 shadow-xl shadow-blue-500/50">
+          <h1 className="text-white font-bold text-3xl w-60 h-15 py-2 text-center border-2 bg-gray-800 border-blue-500 rounded-full m-10 shadow-xl shadow-blue-500/50">
             PROJECTS
           </h1>
         </div>
         <div className="flex justify-center">
           <div className="border border-3 border-blue-700 max-w-7xl w-full h-auto px-25 py-18 bg-gray-800 rounded-2xl shadow-lg">
             <div className="grid grid-cols-3 gap-7">
-              {[...Array(6)].map((_, i) => (
-                <div key={i} className="flex flex-col border border-3 border-blue-700 max-w-xl w-full h-auto px-5 py-5 bg-gray-800 rounded-2xl shadow-lg shadow-blue-500 gap-2">
-                  <h1 className="text-white text-md font-bold">DeepLearning.AI TensorFlow Developer Specialization</h1>
-                  <p className="text-white text-md text-xs">In this certification i learn alot about tensorflow like NLP,Timeseries, and CNN</p>
+              {[
+                { title: "Project will be inserted when cake feel like doing it", 
+                  desc: "Note : maybe" },
+              ].map((cert, index) => (
+                <div key={index} className="flex flex-col border border-3 border-blue-700 max-w-xl w-full h-auto px-5 py-5 bg-gray-800 rounded-2xl shadow-lg shadow-blue-500 gap-2">
+                  <h1 className="text-white text-md font-bold">{cert.title}</h1>
+                  <p className="text-white text-md text-xs">{cert.desc}</p>
                   <a href="#" className="text-white border border-1 w-30 rounded-full border-red-500 bg-red-500 hover:bg-red-500/70 hover:border-red-500/70 px-2 py-1 text-center">🔗Image link</a>
                 </div>
               ))}
             </div>
-          </div>
+          </div>  
         </div>
       </section>
 
